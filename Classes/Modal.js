ed2ffc6aef89643ebfc8595010c94a09f4465684
jamesfondriest <jimifondriest@gmail.com>
2026-03-03 10:25:13 -0500

    Modal class
    nice popup shows up and collects input and resets before next popup.

--- Classes/Modal.js
+++ Classes/Modal.js
@@ -4,36 +4,50 @@ class Modal{
         this.saveButton = this.modal.querySelector(".save-button");
         this.closeButton = this.modal.querySelector(".close-button"); //selects modal from index.html
     }
-    open(){
-        this.modal.style.display = "block";//modal appears
+    open(){//modal appears
         if(this.modal.showModal){
             this.modal.showModal();
-        }
+        }else{
+            this.modal.style.display = "block";
     }
-    close(){
-        this.modal.style.display = "none";//modal disappears
+    close(){//modal disappears
         if(this.modal.close){
             this.modal.close();
-        }
+        } else{
+            this.modal.style.display = "none";
+    }
+        this.resetInput();
     }
     waitForInput(){
-        return new Promise((resolve) => {//modal doesnt wait for user to input before running rest of code, makes function pause for input
-            this.saveButton.addEventListener("click", () => {
-            this.close();
-            resolve(true);
-        }, {once: true});
-        this.closeButton.addEventListener("click", () => {
-            this.close();
-            resolve(false);
-        }, {once: true});
-    });
+        return new Promise((resolve) => {//modal doesnt wait for user to input before continuing running rest of code, this makes the function pause to gather user input
+            const handleSave = () => {
+                clear();
+                this.close();
+                resolve(true);
+            };
+            const handleClose = () => {
+                clear();
+                this.close();
+                resolve(false);
+            };
+            const clear = () => {
+                this.saveButton.removeEventListener("click", handleSave)
+                this.closeButton.removeEventListener("click", handleClose)
+            };
+            this.saveButton.addEventListener("click", handleSave)
+            this.closeButton.addEventListener("click", handleClose)
+        });
+    }
+    resetInput(){
+        const inputs = this.modal.querySelectorAll("input, textarea");
+        inputs.forEach(input => input.value = "");
     }
     static async getNewTask(){  
         //let name = prompt("Input the task name:");
         //let desc = prompt("Input the task's description:");
         let modal = new Modal("myModal")//selects modal ID
         modal.open();
-        const inputReceived = await modal.waitForInput();//pauses for input
+        const inputReceived = await modal.waitForInput();//pauses for input, await applies to async function
         if(inputReceived){//if recieved, create new task
             return new Task(modal.getInputValues("taskName"), modal.getInputValues("taskDesc"));
         }else{
@@ -55,6 +69,7 @@ class Modal{
    getInputValues(input){
         const inputElement = this.modal.querySelector(`#${input}`); //returns value of listname,taskname, taskdesc to create list and task
         if(inputElement){
+            
             return inputElement.value;
         }
         else{
